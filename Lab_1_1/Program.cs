4f5d71199efe1c6ac39839b7138864059ab90578
belinskiy <belinskiy.kr@gmail.com>
2021-06-27 14:14:23 +0300

    (x*y)^3

--- Lab_1_1/Program.cs
+++ Lab_1_1/Program.cs
@@ -6,7 +6,18 @@ namespace Lab_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int first_number;
+            int second_number;
+            int result;
+
+            Console.WriteLine("Enter first number: ");
+            first_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            second_number = Convert.ToInt32(Console.ReadLine());
+
+            result = (int)Math.Pow((first_number * second_number),3);
+            Console.WriteLine("Result is: " + result);
         }
     }
 }
+
