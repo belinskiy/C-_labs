8b00e3d526813a040019a4a277d6486a8f626b64
belinskiy <belinskiy.kr@gmail.com>
2021-06-27 15:59:53 +0300

    Third task of first lab Sum of numbers in array that > 0

---
+++ Lab_1_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_1_3
+{
+    class Program // 5 вариант задачи : Знайти усі елементи матриці, що більші за нуль, та знайти їх суму.
+    {
+        static void Main(string[] args)
+        {
+            int aboveZeroSum = 0;
+            var rand = new Random();
+            int row = 7, column = 5;
+            int[,] arr = new int[row, column];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    arr[i, j] = rand.Next(-10, 10);
+                    Console.Write(arr[i, j] + "\t");
+                    if (arr[i, j] > 0)
+                    {
+                        aboveZeroSum += arr[i, j];
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Sum of numbers in array that > 0 is: " + aboveZeroSum);
+        }
+    }
+}
