491ae511e03c1ec505e8528c0317f0986a55a053
belinskiy <belinskiy.kr@gmail.com>
2021-06-27 15:01:40 +0300

    Second program of first laboratory

---
+++ Lab_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int[] arr = new int[10]; // Initialize integer array
+            for (int i = 0; i < 10; i++) // Fill an array with random integer numbers
+            {
+                arr[i] += rand.Next(100);
+            }
+            Array.Sort(arr); // Sorting the array in ascending order
+            foreach(int value in arr)
+            {
+                Console.WriteLine(value); 
+            }
+        }
+    }
+}
